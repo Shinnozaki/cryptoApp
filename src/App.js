13f888b4ec9ac80a0df232c8497a6eaa55a8e7c1
Joanne Lee <joanne.lee@ids.id>
2023-10-09 10:23:56 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Route, Routes, Link } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Layout, Typography, Space } from 'antd'
+import { Navbar, Homepage, Cryptocurrencies, News, CryptoDetails } from './components'
+
+import './App.css'
+import 'antd/dist/antd.css'
+
+const App = () => {
+    return (
+        <div className='app'>
+
+            <div className='navbar'>
+                <Navbar />
+            </div>
+
+            <div className='main'>
+                <Layout className='routes'>
+                    <Routes>
+                        <Route exact path='/' element={<Homepage />} />
+                        <Route exact path='/cryptocurrencies' element={<Cryptocurrencies />} />
+                        <Route exact path='/crypto/:coinId' element={<CryptoDetails />} />
+                        <Route exact path='/news' element={<News />} />
+                    </Routes>
+                </Layout>
+                <div className='footer'>
+                    <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>
+                        Cryptoverse <br />
+                        All rights reserved
+                    </Typography.Title>
+                    <Space>
+                        <Link to="/">Home</Link>
+                        <Link to="/exchanges">Exchanges</Link>
+                        <Link to="/news">News</Link>
+                    </Space>
+                </div>
+            </div>
+
+        </div>
+    )
 }
 
-export default App;
+export default App
